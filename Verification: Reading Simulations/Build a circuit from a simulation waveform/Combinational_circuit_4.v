28541a87447487d5512f80ee83e0157bdc19bef7
Michele Missiroli <163904638+hinnhi@users.noreply.github.com>
2026-06-14 15:41:37 +0200

    Create Combinational_circuit_4.v

---
+++ Verification: Reading Simulations/Build a circuit from a simulation waveform/Combinational_circuit_4.v
@@ -0,0 +1,24 @@
+/*
+module top_module (
+    input a,
+    input b,
+    input c,
+    input d,
+    output q );//
+
+    assign q = 0; // Fix me
+
+endmodule
+*/
+
+module top_module (
+    input a,
+    input b,
+    input c,
+    input d,
+    output q 
+  );
+
+    assign q = b | c;
+
+endmodule
